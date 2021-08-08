84f85099a859777c7c0177fa8d2eb190b2b5bd1a
YuseiNaito <yusei.vitz.787@gmail.com>
2021-08-08 17:16:05 +0900

    現在時刻を表示

--- clock.py
+++ clock.py
@@ -1,13 +1,20 @@
+from gi.repository import Gtk
+import numpy as np
+import math
 import cairo
+import datetime
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-import math
-import os
-import subprocess
-import sys
 
-class DrawingAreaFrame(Gtk.Frame):
+
+def rotation2d(theta):
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+    return np.array([[cos, -sin],
+                     [sin,  cos]])
+
+
+class clock_area(Gtk.Frame):
     def __init__(self, css=None, border_width=0):
         super().__init__()
         self.set_border_width(border_width)
@@ -31,24 +38,51 @@ class DrawingAreaFrame(Gtk.Frame):
         field_height = 2.0 * (rad + line_width)
 
         coef = min(aw / field_width, ah / field_height)
-        cr.transform(cairo.Matrix(coef, 0, 0, -coef, coef * field_width / 2.0, coef * field_height / 2.0))
-    
-        cr.set_line_width(line_width)
+        cr.transform(cairo.Matrix(coef, 0, 0, -coef, coef *
+                     field_width / 2.0, coef * field_height / 2.0))
+
         cr.set_source_rgb(1.0, 1.0, 1.0)
+        cr.rectangle(-field_width / 2.0, -field_height / 2.0, field_width, field_height)
+        cr.fill()
+
+        cr.set_source_rgb(0.0, 0.0, 0.0)
+
+        # circle
+        cr.set_line_width(line_width)
         cr.arc(0.0, 0.0, rad, 0.0, 2 * math.pi)
         cr.stroke()
+
+        now = datetime.datetime.now()
+
+        # long hand
+        target = np.dot(rotation2d(- (now.minute / 60.0) *
+                        2.0 * math.pi), np.array([0.0, 0.9 * rad]))
+        cr.set_line_width(0.25 * line_width)
+        cr.move_to(0, 0)
+        cr.line_to(target[0], target[1])
+        cr.stroke()
+
+        # short hand
+        target = np.dot(rotation2d(-((now.hour % 12) / 12) *
+                        2.0 * math.pi - (now.minute / 60.0) * math.pi / 12.0), np.array([0.0, 0.5 * rad]))
+        cr.set_line_width(0.5 * line_width)
+        cr.move_to(0, 0)
+        cr.line_to(target[0], target[1])
+        cr.stroke()
         return False
- 
-class Window(Gtk.Window):
+
+
+class window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
         self.set_title("clock")
         self.set_default_size(800, 600)
         self.connect("destroy", Gtk.main_quit)
 
-        frame = DrawingAreaFrame()
-        self.add(frame)
+        ca = clock_area()
+        self.add(ca)
 
-window = Window()
+
+window = window()
 window.show_all()
 Gtk.main()
