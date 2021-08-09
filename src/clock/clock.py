d4f277b3659ddd4b3874ddedf7fec843316ad722
YuseiNaito <yusei.vitz.787@gmail.com>
2021-08-10 06:09:21 +0900

    lock 導入

--- src/clock/clock.py
+++ src/clock/clock.py
@@ -1,23 +1,29 @@
 import datetime
 import math
+import threading
 
 
 class Clock():
     def __init__(self):
         self.__dir_long = 0.0
         self.__dir_short = 0.0
+        self.__lock = threading.RLock()
 
     def dir_long(self):
-        return self.__dir_long
+        with self.__lock:
+            return self.__dir_long
 
     def dir_short(self):
-        return self.__dir_short
+        with self.__lock:
+            return self.__dir_short
 
     def set_time(self, time):
-        self.__dir_long = - (time.minute / 60.0) * 2.0 * \
-            math.pi + math.pi / 2.0
-        self.__dir_short = -((time.hour % 12) / 12) * 2.0 * math.pi - \
-            (time.minute / 60.0) * math.pi / 12.0 + math.pi / 2.0
+        with self.__lock:
+            self.__dir_long = - (time.minute / 60.0) * 2.0 * \
+                math.pi + math.pi / 2.0
+            self.__dir_short = -((time.hour % 12) / 12) * 2.0 * math.pi - \
+                (time.minute / 60.0) * math.pi / 12.0 + math.pi / 2.0
 
     def set_now(self):
-        self.set_time(datetime.datetime.now())
+        with self.__lock:
+            self.set_time(datetime.datetime.now())
