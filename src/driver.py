16824dcc6a090285609801419c3fec67c6135623
YuseiNaito <yusei.vitz.787@gmail.com>
2021-08-10 12:22:09 +0900

    Demo を動かす

--- src/driver.py
+++ src/driver.py
@@ -2,11 +2,11 @@ import os
 import sys
 import time
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.action import now  # nopep8
+from src.action import demo  # nopep8
 
 
 def run(clk, cycle):
-    action = now.Now(clk)
+    action = demo.Demo(clk)
     while True:
         start = time.time()
         action.execute()
