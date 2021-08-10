e27454ff7e83558ec2373f6b7661b80acd73d73e
YuseiNaito <yusei.vitz.787@gmail.com>
2021-08-10 10:28:26 +0900

    速度指令してみる

--- src/driver.py
+++ src/driver.py
@@ -1,10 +1,14 @@
+import os
+import sys
 import time
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.clock import clock  # nopep8
 
 
 def run(clk, cycle):
     while True:
         start = time.time()
-        clk.set_now()
+        clk.set_target_vel(clock.Dir(long=1.0, short=-1.0))
         clk.execute()
 
         time.sleep(max(0.0, cycle - (time.time() - start)))
