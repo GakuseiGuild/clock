fccfa270cf28ce1c53c3925741cae265fa0fcac0
YuseiNaito <yusei.vitz.787@gmail.com>
2021-09-05 00:32:28 +0900

    パスを修正

--- app/test.py
+++ app/test.py
@@ -4,7 +4,7 @@ import logging
 import sys
 import os
 libdir = os.path.join(os.path.dirname(os.path.dirname(
-    os.path.realpath(__file__))), "e-Paper/RaspberryPi_JetsonNano/python/lib")
+    os.path.realpath(__file__))), "../e-Paper/RaspberryPi_JetsonNano/python/lib")
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
