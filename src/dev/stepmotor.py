cd4216b3f61bfb594c955e69c1a38b03885d128e
Taisuke Tane <31725561+tanechai@users.noreply.github.com>
2021-12-28 21:58:28 +0900

    Add some comments.

--- src/dev/stepmotor.py
+++ src/dev/stepmotor.py
@@ -5,13 +5,16 @@ import warnings
 import math
 from typing import NamedTuple
 
+# ステッピングモーターのピン番号構造
 class step_motor_pin_number(NamedTuple):
     A: int
     B: int
     C: int
     D: int
 
+# ステッピングモーターのクラス
 class step_motor():
+    # 初期化
     def __init__(self,pin:step_motor_pin_number):
         self.__pin = pin
         self.__pi1 = pigpio.pi()
@@ -23,17 +26,21 @@ class step_motor():
         self.__angular_velocity = 0.0
         self.__angular_velocity_max = 20*math.pi/4.0
         self.__lock = threading.RLock()
+        # 各ステップでのピン出力の定義
         self.__state = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
+        # stateのどの状態かカウントして記憶するための変数
         self.__count = 0
 
     def __del__(self):
         pass
     
+    # 角速度取得
     @property
     def angular_velocity(self) -> float:
         with self.__lock:
             return self.__angular_velocity
     
+    # 角速度設定
     @angular_velocity.setter
     def angular_velocity(self,angular_velocity:float):
         with self.__lock:
@@ -43,6 +50,7 @@ class step_motor():
             else:
                 self.__angular_velocity = angular_velocity
     
+    # 1ステップ回転
     def __rotate_one_step(self,clockwise:bool):
         if clockwise:
             self.__count += 1
@@ -54,12 +62,14 @@ class step_motor():
                 self.__count = 7
         self.__write()
 
+    # 各ピンに出力
     def __write(self):
         self.__pi1.write(self.__pin.A,self.__state[self.__count][0])
         self.__pi1.write(self.__pin.B,self.__state[self.__count][1])
         self.__pi1.write(self.__pin.C,self.__state[self.__count][2])
         self.__pi1.write(self.__pin.D,self.__state[self.__count][3])
 
+    # ループ
     def start(self):
         while True:
             with self.__lock:
