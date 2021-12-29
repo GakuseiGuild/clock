ea39269eabee7c982142d65d5d46dbc27ef0bc98
kambe <nori.k3636@gmail.com>
2021-12-29 20:31:19 +0900

    精度を上げる

--- julius/hook.py
+++ julius/hook.py
@@ -12,17 +12,24 @@ while True:
         res += sock.recv(1024).decode()
 
     word = ''
+    cm = ''
     for line in res.split('\n'):
         # Juliusから取得した値から認識文字列の行を探す
         index = line.find('WORD=')
+        cmdex = line.find('CM=')
         if index != -1:
             # 認識文字列部分だけを抜き取る
-            line = line[index + 6 : line.find('"', index + 6)]
+            line_wd = line[index + 6 : line.find('"', index + 6)]
+            line_cm = line[cmdex + 4 : line.find('"', cmdex + 6)]
             # 文字列の開始記号以外を格納していく
-            if line != '[s]':
-                word = word + line
+            if line_wd != '[s]':
+                word = word + line_wd
+                cm = cm + line_cm
                 break
     if word != '':
         # ここで単語が出てくるので，ifなりなんなりで分岐させて処理を行う
         print(word)
+        print(cm)
+        if(cm == "1.000"):
+            print('成功！')
     res = ''
