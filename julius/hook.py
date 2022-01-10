78117e130e1bffae8d736344fa29488aae88cbdd
kambe <nori.k3636@gmail.com>
2022-01-10 14:02:38 +0900

    julius実行の変更

--- julius/hook.py
+++ julius/hook.py
@@ -1,3 +1,4 @@
+# coding: UTF-8
 import socket
 
 # Juliusに接続する準備
@@ -9,27 +10,29 @@ while True:
     # 音声認識の区切りである「改行+.」がくるまで待つ
     while (res.find('\n.') == -1):
         # Juliusから取得した値を格納していく
-        res += sock.recv(1024).decode()
-
+        res += sock.recv(1024).decode('utf-8')
+	#print("res"+res)
     word = ''
-    cm = ''
-    for line in res.split('\n'):
+    for line in res.split('/>'):
         # Juliusから取得した値から認識文字列の行を探す
-        index = line.find('WORD=')
-        cmdex = line.find('CM=')
+	#print("line:"+line)
+	index = line.find('WORD=')
         if index != -1:
             # 認識文字列部分だけを抜き取る
-            line_wd = line[index + 6 : line.find('"', index + 6)]
-            line_cm = line[cmdex + 4 : line.find('"', cmdex + 6)]
+            recog = line[index + 6 : line.find('"', index + 6)]
             # 文字列の開始記号以外を格納していく
-            if line_wd != '[s]':
-                word = word + line_wd
-                cm = cm + line_cm
-                break
+	    if recog != "を".decode('utf-8'):
+            	word = word + recog
     if word != '':
+	word = word[3:-4]
         # ここで単語が出てくるので，ifなりなんなりで分岐させて処理を行う
-        print(word)
-        print(cm)
-        if(cm == "1.000"):
-            print('成功！')
+        print("word"+word)
+	if word == "あぶらかたぶら音楽鳴らして".decode('utf-8'):
+		print("音楽を流す")
+	if word == "あぶらかたぶら時刻教えて".decode('utf-8'):
+                print("時刻を表示する")
+	if word == "あぶらかたぶら干支教えて".decode('utf-8'):
+                print("干支を表示する")
+	if word == "あぶらかたぶら月齢教えて".decode('utf-8'):
+                print("月齢を表示する")
     res = ''
